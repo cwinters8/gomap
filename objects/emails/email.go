4cc688106ddd88f30d00a6788fff264dc54c3cb7
Clark Winters <github@clarkwinters.com>
2023-01-04 19:47:42 -0600

    move Email.Set to its own file

--- objects/emails/email.go
+++ objects/emails/email.go
@@ -3,9 +3,6 @@ package emails
 import (
 	"fmt"
 
-	"github.com/cwinters8/gomap/requests"
-	"github.com/cwinters8/gomap/utils"
-
 	"github.com/google/uuid"
 )
 
@@ -62,61 +59,6 @@ func NewEmail(boxIDs []string, from []*Address, to []*Address, subject, body str
 			ID:    bodyID,
 			Type:  bodyType,
 			Value: body,
-		},
-	}, nil
-}
-
-func (e *Email) Set(acctID string) (*requests.Call, error) {
-	id, err := uuid.NewRandom()
-	if err != nil {
-		return nil, fmt.Errorf("failed to generate new uuid: %w", err)
-	}
-	reqID, err := uuid.NewRandom()
-	if err != nil {
-		return nil, fmt.Errorf("failed to generate new uuid: %w", err)
-	}
-	mailboxes := map[string]bool{}
-	for _, box := range e.MailboxIDs {
-		mailboxes[box] = true
-	}
-	return &requests.Call{
-		ID:        id,
-		AccountID: acctID,
-		Method:    "Email/set",
-		Arguments: map[string]any{
-			"create": map[string]map[string]any{
-				reqID.String(): {
-					"mailboxIds": mailboxes,
-					"from":       e.From,
-					"to":         e.To,
-					"subject":    e.Subject,
-					"bodyStructure": map[string]string{
-						"partId": e.Body.ID.String(),
-						"type":   string(e.Body.Type),
-					},
-					"bodyValues": map[string]map[string]string{
-						e.Body.ID.String(): {
-							"value": e.Body.Value,
-						},
-					},
-				},
-			},
-		},
-		OnSuccess: func(m map[string]any) error {
-			created, ok := m["created"].(map[string]any)
-			if !ok {
-				return fmt.Errorf("failed to cast created value to map. %s", utils.Describe(m["created"]))
-			}
-			value, ok := created[reqID.String()].(map[string]any)
-			if !ok {
-				return fmt.Errorf("failed to cast result value to map. %s", err.Error())
-			}
-			resultID, ok := value["id"].(string)
-			if !ok {
-				return fmt.Errorf("failed to cast id to string. %s", err.Error())
-			}
-			e.ID = resultID
-			return nil
 		},
 	}, nil
 }
