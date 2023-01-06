252750366c437adbf5b3fdb85691c7268591e19b
Clark Winters <github@clarkwinters.com>
2023-01-05 20:10:58 -0600

    handle case of file already existing

--- utils/debug.go
+++ utils/debug.go
@@ -2,8 +2,10 @@ package utils
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"os"
+	"strconv"
 	"time"
 )
 
@@ -26,7 +28,31 @@ func WriteJSON(name, dir string, raw map[string][]byte) error {
 		return fmt.Errorf("failed to marshal document to json: %w", err)
 	}
 	// timestamp format is RFC3339 with more precision
-	filename := fmt.Sprintf("%s/%s-%s.json", dir, name, t.Format("2006-01-02T15:04:05.99Z07:00"))
+	prefix := fmt.Sprintf("%s/%s-%s", dir, name, t.Format("2006-01-02T15:04:05.99Z07:00"))
+	suffix := "json"
+	filename := fmt.Sprintf("%s.%s", prefix, suffix)
+	// check if file exists before writing
+	for {
+		if _, err := os.Stat(filename); err == nil {
+			idx := len(prefix) - 1
+			endStr := string(prefix[idx])
+			end, err := strconv.Atoi(endStr)
+			if err != nil {
+				return fmt.Errorf("failed to convert character `%s` to int: %w", endStr, err)
+			}
+			switch end {
+			case 0:
+				prefix += "-1"
+			default:
+				prefix = fmt.Sprintf("%s%d", prefix[:idx-1], end+1)
+			}
+			filename = fmt.Sprintf("%s.%s", prefix, suffix)
+		} else if errors.Is(err, os.ErrNotExist) {
+			break
+		} else {
+			return fmt.Errorf("something went wrong with checking os.Stat on file `%s`: %w", filename, err)
+		}
+	}
 	if err := os.WriteFile(filename, b, 0644); err != nil {
 		return fmt.Errorf("failed to write to file `%s`: %w", filename, err)
 	}
