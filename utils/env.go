eaee9d6dee393e980ab24373bafe682cdb42c215
Clark Winters <github@clarkwinters.com>
2026-06-03 16:02:55 -0500

    add env variables for tests and bump go version

--- utils/env.go
+++ utils/env.go
@@ -1,10 +1,15 @@
 package utils
 
 import (
+	"os"
+
 	"github.com/joho/godotenv"
 )
 
 // load environment variables from path
 func Env(path string) error {
-	return godotenv.Load(path)
+	if err := godotenv.Load(path); err != nil && !os.IsNotExist(err) {
+		return err
+	}
+	return nil
 }
